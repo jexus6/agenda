93b21bbaf4b78c37a38a236d8aa2eb1c81b80a9b
Jesus Parra <jesusfernando.parra@altran.com>
2017-11-25 13:41:44 +0100

    primer commit

--- src/app/contactos.service.ts
+++ src/app/contactos.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Contacto } from './contacto';
 
 @Injectable()
 export class ContactosService {
 
-   private _listaContactos: string [] = [
-        'Bill Gates',
-        'Tim Cook',
-        'Elon Musk',
-        'Steve Wozniak',
-        'Sundar Pichai'
+   private _listaContactos: Contacto [] = [
+        {nombre: 'Bill ', apellidos: 'Gates'},
+        {nombre: 'Tim ', apellidos: 'Cook'},
+        {nombre: 'Elon ', apellidos: 'Musk'},
+        {nombre: 'Steve ', apellidos: 'Wozniak'},
+        {nombre: 'Sundar ', apellidos: 'Pichai'}
       ];
 
-   obtenerContactos(): string []{
+   obtenerContactos(): string [] {
         return this._listaContactos;
-   }   
-
-   crearContacto(contacto :string) : void {
+   }
+   crearContacto(contacto: string): void {
      this._listaContactos.push(contacto);
    }
 
